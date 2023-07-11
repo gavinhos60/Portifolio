749fa1cdd338d4ecc74a1291a695a813eb739136
Rafael <gavinhos60@gmail.com>
2023-07-11 12:41:02 -0300

    primeiro commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../style.css/header.css'
+
+const Header = () => {
+  return (
+    <div>
+        <header>teste</header>
+    </div>
+  )
+}
+
+export default Header;
